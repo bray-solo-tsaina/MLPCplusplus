1a5a5066c9b02535b9b4bd4729c48e73372ce317
Brayden Smith <smithmpanefy@gmail.com>
2024-06-05 11:36:03 -0700

    adding comment check

---
+++ .github/workflows/comment-find.js
@@ -0,0 +1,23 @@
+module.exports = async ({github, context}) => {
+    const {owner, repo} = context.repo;
+    const pull_number = context.issue.number;
+
+    const {status, data: pr} = await github.rest.pulls.listFiles({owner, repo, pull_number});
+    if (status !== 200) {
+        throw new Error(`Failed to fetch PR: ${pull_number}`);
+    }
+
+    const svcReachable = [
+        "cmd/",
+    ]
+
+    for (let i in pr) {
+        for (let j in svcReachable) {
+            if (pr[i].filename.includes(svcReachable[j])) {
+                return true
+            }
+        }
+    }
+
+    return false
+}
